b2e7faecb2ffbbd1cf76510b6cb13c9510f09352
mario <marioantonioportilloromero@gmail.com>
2026-02-03 18:49:30 -0600

    nuevo

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Middleware\DelayRequest;
+use App\Http\Middleware\VerificarIPs;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
@@ -7,11 +9,16 @@ use Illuminate\Foundation\Configuration\Middleware;
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
         web: __DIR__.'/../routes/web.php',
+        api: __DIR__.'/../routes/api.php',
         commands: __DIR__.'/../routes/console.php',
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-        //
+        $middleware->alias([
+        'verificar.ip' => VerificarIPs::class,
+
+        'delay.game' => DelayRequest::class,
+            ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
