f6b5eb8b1b42d41ed99a73e49926523826f2d24c
angelgabriel2030 <gabrielcampirano@gmail.com>
2026-02-18 07:20:22 -0600

    Commit

--- app/Http/Controllers/JuegoController.php
+++ app/Http/Controllers/JuegoController.php
@@ -10,11 +10,7 @@ use Illuminate\Support\Facades\http;
 class JuegoController extends Controller
 {
 
-<<<<<<< HEAD
     private string $ipPermitida = 'https://azariah-unbrittle-gwen.ngrok-free.dev';
-=======
-    private string $ipPermitida = 'https://roni-promodernistic-depreciatingly.ngrok-free.dev';
->>>>>>> bafffe825d326e44ec8a0f02b2d3f28b15529d20
 
     public function paparecibida(Request $request)
     {
@@ -48,16 +44,12 @@ class JuegoController extends Controller
         }
     }
 
-    public function enviarpapa(int $numero)
-    {
-        try {
-<<<<<<< HEAD
-            $response = Http::timeout(10)->post($this->ipPermitida, [
-=======
-            $response = Http::timeout(5)->poat($this->ipPermitida, [
->>>>>>> bafffe825d326e44ec8a0f02b2d3f28b15529d20
-                'numero' => $numero
-            ]);
+public function enviarpapa(int $numero)
+{
+    try {
+        $response = Http::timeout(10)->post($this->ipPermitida, [
+            'numero' => $numero
+        ]);
 
             if ($response->successful()) {
                 return true;
@@ -103,18 +95,10 @@ class JuegoController extends Controller
         $estado = DB::table('game_state')->find(1);
 
         return response()->json([
-<<<<<<< HEAD
             'laptop'=> 3,
-=======
-            'laptop'=> 1,
->>>>>>> bafffe825d326e44ec8a0f02b2d3f28b15529d20
             'numero_actual' => $estado->numero ?? 0,
             'numero_maximo' => $estado->num_max ?? 0,
             'updated_at' => $estado->updated_at ?? null,
         ]);
     }
-<<<<<<< HEAD
 }
-=======
-}
->>>>>>> bafffe825d326e44ec8a0f02b2d3f28b15529d20
